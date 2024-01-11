bfd755b9045270d4363b86a1a75a7a4570da4898
Mario <mario2002pc@hotmail.com>
2024-01-11 08:27:39 +0000

    Creation of interface Command and PowerCommand

--- src/main/java/Kata5/Command.java
+++ src/main/java/Kata5/Command.java
@@ -1,4 +1,28 @@
 package Kata5;
 
 public interface Command {
+    Output execute (Input input);
+
+    interface Input {
+        String key(String value);
+    }
+
+    interface Output {
+        int code();
+        String Resul();
+    }
+
+    static Output output(int code, String Resul) {
+        return new Output() {
+            @Override
+            public int code() {
+                return code;
+            }
+
+            @Override
+            public String Resul() {
+                return Resul;
+            }
+        };
+    }
 }
--- src/main/java/Kata5/PowerCommand.java
+++ src/main/java/Kata5/PowerCommand.java
@@ -1,4 +1,32 @@
 package Kata5;
 
-public class PowerCommand {
+import static java.lang.Integer.parseInt;
+
+public class PowerCommand implements Command{
+    @Override
+    public Output execute(Input input) {
+        try {
+            return power(input.key("value"), input.key("number"));
+        } catch (NumberFormatException e) {
+            return Command.output(400, "Algun valor proporcionado no es valido");
+        }
+    }
+
+    private Output power(String value, String number) {
+        try {
+            int v = parseInt(value);
+            int n = parseInt(number);
+            return powerOperation(v, n);
+        } catch (NumberFormatException e) {
+            return Command.output(400, "Algun valor proporcionado no es valido");
+        }
+    }
+
+    private Output powerOperation(int v, int n) {
+        if (v > 10 || n > 1000 || v < -10 || n < -1000) {
+            return Command.output(400, "Algun valor proporcionado es demasiado grande o pequeño para hacer los calculos (Limites: Valor: -1000-1000| Potencia: -10-10)");
+        }
+        double power = Math.pow(v, n);
+        return Command.output(200, String.valueOf(power));
+    }
 }
