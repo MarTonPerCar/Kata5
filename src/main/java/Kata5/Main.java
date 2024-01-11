f86868adf1bf2c6e69ca14641b3f6d189e6adb3a
Mario <mario2002pc@hotmail.com>
2024-01-11 08:45:39 +0000

    Finish of Main and Client

--- src/main/java/Kata5/Main.java
+++ src/main/java/Kata5/Main.java
@@ -1,13 +1,11 @@
 package Kata5;
 
-import spark.Request;
-import spark.Response;
 import spark.Spark;
 
 public class Main {
     public static void main (String[] args) {
         Spark.port(8081);
-        Spark.get("/factorial", ((request, response) -> "Potencia de los dos valores de la URL: " +
+        Spark.get("/power", ((request, response) -> "Potencia de los dos valores de la URL: " +
                 new CommandExecutor(request, response).execute(new PowerCommand())));
     }
 }
